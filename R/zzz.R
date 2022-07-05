e15d184e6089a78ecba74361393bc24ac3e4fb5c
Mauricio Vargas <mavargas11@uc.cl>
2022-07-04 20:19:59 -0400

    new contact

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(...) {
+  message("La documentacion del paquete y ejemplos de uso se encuentran en https://pacha.dev/chilemapas/.")
+  message("Visita https://buymeacoffee.com/pacha/ si deseas donar para contribuir al desarrollo de este software.")
+}
